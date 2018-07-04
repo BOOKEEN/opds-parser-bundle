5c606b7cd6ed9923524b2f019e0e2900d9bfc050
Thomas GACHET <thomas_gachet@bookeen.com>
2018-07-04 19:06:57 +0200

    MBW-1860 delete comp.lock

--- DependencyInjection/OpdsParserExtension.php
+++ DependencyInjection/OpdsParserExtension.php
@@ -3,9 +3,9 @@
 namespace OpdsParser\OpdsBundle\DependencyInjection;
 
 use Symfony\Component\Config\FileLocator;
-use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
 class OpdsParserExtension extends Extension
 {
